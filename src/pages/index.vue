26e9ec955cfb7f4540425abba82af6ce94a4d919
凡生 <orochitian@126.com>
2020-12-29 17:37:35 +0800

    ryan
    首页

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,96 @@
 <template>
   <div class="container">
-    首页
+    <el-row :gutter="20">
+      <el-col :md="6">
+        <el-card class="box-card">
+          <div slot="header">
+            <span>产量</span>
+            <el-tag style="float: right;" size="small">当月</el-tag>
+          </div>
+          <div class="body">
+            <div class="el-icon-s-data img"></div>
+            <div class="count">723845</div>
+            <div class="unit">吨</div>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :md="6">
+        <el-card class="box-card">
+          <div slot="header">
+            <span>盘数</span>
+            <el-tag style="float: right;" size="small">当月</el-tag>
+          </div>
+          <div class="body">
+            <div class="el-icon-pie-chart img"></div>
+            <div class="count">723845</div>
+            <div class="unit">吨</div>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :md="6">
+        <el-card class="box-card">
+          <div slot="header">
+            <span>超差</span>
+            <el-tag style="float: right;" type="warning" size="small">当月</el-tag>
+          </div>
+          <div class="body">
+            <div class="el-icon-warning img" style="color: #e6a23c;"></div>
+            <div class="count">723845</div>
+            <div class="unit">吨</div>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :md="6">
+        <el-card class="box-card">
+          <div slot="header">
+            <span>短信</span>
+            <el-tag style="float: right;" type="danger" size="small">报警</el-tag>
+          </div>
+          <div class="body">
+            <div class="el-icon-message img" style="color: #f56c6c;"></div>
+            <div class="count">723845</div>
+            <div class="unit" style="color: #f56c6c;">吨</div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  methods: {
+    turn() {},
+  },
+};
 </script>
 
-<style>
-
+<style scoped lang="scss">
+  .index{
+    color: red;
+  }
+  .box-card{
+    .body{
+      vertical-align: middle;
+      .img{
+        font-size: 40px;
+        color: #1ab394;
+        vertical-align: middle;
+      }
+      .count{
+        display: inline-block;
+        vertical-align: middle;
+        font-size: 30px;
+        margin-left: 50px;
+        color: #444;
+      }
+      .unit{
+        display: inline-block;
+        margin-left: 40px;
+        vertical-align: bottom;
+        font-weight: bold;
+        color: #E6A23C;
+      }
+    }
+  }
 </style>
--- src/pages/layout.vue
+++ src/pages/layout.vue
@@ -1,48 +1,43 @@
 <template>
   <el-container>
-    <el-aside width="210px">
-      <el-menu 
-        :default-openeds="['1', '2']"
-        background-color="#545c64"
-        text-color="#fff"
-        active-text-color="#ffd04b"
-      >
-        <el-submenu index="1">
-          <template slot="title"
-            ><i class="el-icon-message"></i>导航一</template
-          >
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="1-4-1">选项4-1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-        <el-submenu index="2">
-          <template slot="title"><i class="el-icon-menu"></i>导航二</template>
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="2-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="2-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-      </el-menu>
-    </el-aside>
+    <el-header id="header">公路施工质量实时数据监测平台</el-header>
     <el-container>
-      <el-header>Header</el-header>
+      <el-aside width="210px">
+        <el-menu
+          default-active="/index"
+          :default-openeds="['2', '3']"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          :router="true"
+        >
+          <el-menu-item index="/index">
+            <i class="el-icon-menu"></i>
+            <span slot="title">首页</span>
+          </el-menu-item>
+          <el-submenu index="2">
+            <template slot="title"
+              ><i class="el-icon-message"></i>动态数据</template
+            >
+            <el-menu-item-group>
+              <el-menu-item index="/test1">水稳拌合站</el-menu-item>
+              <el-menu-item index="/test2">改性沥青站</el-menu-item>
+              <el-menu-item index="/test3">沥青拌合站</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title"
+              ><i class="el-icon-menu"></i>综合分析</template
+            >
+            <el-menu-item-group>
+              <el-menu-item index="2-1">水稳拌合统计</el-menu-item>
+              <el-menu-item index="2-2">改性沥青统计</el-menu-item>
+              <el-menu-item index="2-2">沥青拌合统计</el-menu-item>
+              <el-menu-item index="2-2">沥青超差统计</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-main>
         <router-view></router-view>
       </el-main>
@@ -54,8 +49,17 @@
 export default {};
 </script>
 
+<style scoped>
+#header {
+  background-color: #43464b;
+  line-height: 60px;
+  font-size: 20px;
+  color: #eee;
+}
+</style>
+
 <style>
-  body{
-    margin: 0;
-  }
+body {
+  margin: 0;
+}
 </style>
